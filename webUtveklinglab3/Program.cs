1db0c50de4037cca5d8a0a9dea40f4c70bb29824
Yazen96 <yazan.alnsierat@iths.se>
2025-03-20 22:01:21 +0100

    Refactor connection string retrieval in Program.cs
    Updated the method of fetching the SQL Server connection string
    in Program.cs from using builder.Configuration["DefaultConnection"]
    to builder.Configuration.GetConnectionString("DefaultConnection").
    This change enhances clarity and aligns with best practices for
    configuration management.

--- webUtveklinglab3/Program.cs
+++ webUtveklinglab3/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationContext>(options =>
-    options.UseSqlServer(builder.Configuration["DefaultConnection"]));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 //builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 
